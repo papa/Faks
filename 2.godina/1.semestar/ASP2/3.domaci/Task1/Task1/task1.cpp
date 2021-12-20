432afce89092c8ec328e337f6c963ba64e6ff7db
papa <pavle.jane@gmail.com>
2021-12-20 23:53:42 +0100

    ASP2 3.domaci

---
+++ 2.godina/1.semestar/ASP2/3.domaci/Task1/Task1/task1.cpp
@@ -0,0 +1,319 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+//probaj ovde vise stvari
+const int prime1 = (1e9) + 7;
+const int prime2 = 1299709;
+
+class AdressFunction;
+class SplitSequenceLinearHashing;
+class Student;
+class HashTable;
+
+class HashTable
+{
+	int hashTableSize = 0;
+	int numberOfKeys = 0;
+	int p;
+	vector<Student*> values;
+	SplitSequenceLinearHashing* collisionH = nullptr;
+public:
+	vector<int> keys;
+	vector<int> mark;
+	friend SplitSequenceLinearHashing;
+	static const int EMPTY = -1;
+	static const int DELETED = 0;
+	static const int FULL = 1;
+
+	HashTable(int k, int p);
+
+	int findKeyPos(int key);
+
+	Student* findKey(int key);
+
+	int originalHash(int key)
+	{
+		return key % (1 << p);
+	}
+
+	bool insertKey(int key, Student* student);
+
+	bool deleteKey(int key);
+
+	void clear();
+
+	int keyCount()
+	{
+		return numberOfKeys;
+	}
+
+	int tableSize()
+	{
+		return hashTableSize;
+	}
+
+	friend ostream& operator << (ostream& os, const HashTable& hashTable)
+	{
+		os << "HashTable print" << endl;
+		for (int i = 0; i < hashTable.hashTableSize;i++)
+		{
+			cout << i << " ";
+			if (hashTable.mark[i] == HashTable::EMPTY)
+			{
+				cout << "EMPTY" << endl;
+			}
+			else if (hashTable.mark[i] == HashTable::DELETED)
+			{
+				cout << "DELETED" << endl;
+			}
+			else
+			{
+				cout << hashTable.keys[i] << endl;
+			}
+		}
+		return os;
+	}
+
+	double fillRatio()
+	{
+		return numberOfKeys / hashTableSize;
+	}
+
+	~HashTable()
+	{
+		delete collisionH;
+		clear();
+	}
+};
+
+class AdressFunction
+{
+public:
+	virtual int getAdress(int key, int adr, int attempt, int sz, const HashTable& hastTable) = 0;
+};
+
+class SplitSequenceLinearHashing : public AdressFunction
+{
+private:
+	int s1, s2;
+public:
+	SplitSequenceLinearHashing(int s1, int s2) : s1(s1), s2(s2) {}
+
+	int getAdress(int key, int adr, int attempt, int sz, const HashTable& hashTable) override
+	{
+		if (hashTable.keys[adr] < key)
+			return (1LL*adr + 1LL*s1 * attempt) % sz;
+		
+		return (adr + 1LL* s2 * attempt) % sz;
+	}
+};
+
+class Student
+{
+private:
+	int indeks;
+	vector<string> predmeti;
+	string ime;
+	string prezime;
+public:
+	Student(int ind, const string& im, const string& prez, const vector<string>& p) :
+		indeks(ind), ime(im), prezime(prez)
+	{
+		for (int i = 0; i < p.size();i++)
+			predmeti.push_back(p[i]);
+	}
+
+	friend ostream& operator << (ostream& os, const Student& s)
+	{
+		cout << "Podaci o studentu" << endl;
+		cout << "indeks: " << s.indeks << endl;
+		cout << "Ime i prezime: " << s.ime << " " << s.prezime << endl;
+		cout << "Predmeti koje slusa: " << endl;
+		for (int i = 0;i < s.predmeti.size();i++)
+			cout << s.predmeti[i] << endl;
+		return os;
+	}
+};
+
+HashTable::HashTable(int k, int p) : hashTableSize(k), p(p)
+{
+	collisionH = new SplitSequenceLinearHashing(prime1, prime2); // podesi ove korake
+	keys.resize(hashTableSize);
+	mark.resize(hashTableSize);
+	values.resize(hashTableSize);
+	for (int i = 0; i < hashTableSize;i++)
+	{
+		keys[i] = EMPTY;
+		mark[i] = EMPTY;
+		values[i] = nullptr;
+	}
+}
+
+int HashTable::findKeyPos(int key)
+{
+	int att = 0;
+	int adr;
+	do
+	{
+		if (att == 0)
+		{
+			adr = originalHash(key);
+		}
+		else
+		{
+			adr = collisionH->getAdress(key, adr, att, hashTableSize, *this);
+		}
+
+		if (mark[adr] != EMPTY && mark[adr] != DELETED && keys[adr] == key)
+			return adr;
+
+	} while (mark[adr] != EMPTY && att < hashTableSize);
+	return -1;
+}
+
+Student* HashTable::findKey(int key)
+{
+	int pos = findKeyPos(key);
+	if (pos == -1) return nullptr;
+	return values[pos];
+}
+
+bool HashTable::deleteKey(int key)
+{
+	int pos = findKeyPos(key);
+	if (pos == -1) return false;
+	mark[pos] = DELETED;
+	keys[pos] = DELETED;
+	delete values[pos];
+	values[pos] = nullptr;
+	numberOfKeys--;
+	return true;
+}
+
+void HashTable::clear()
+{
+	for (int i = 0; i < hashTableSize;i++)
+	{
+		mark[i] = EMPTY;
+		keys[i] = EMPTY;
+		delete values[i];
+		values[i] = nullptr;
+	}
+	numberOfKeys = 0;
+}
+
+bool HashTable::insertKey(int key, Student* student)
+{
+	int att = 0;
+	int adr;
+	do
+	{
+		if (att == 0)
+		{
+			adr = originalHash(key);
+		}
+		else
+		{
+			adr = collisionH->getAdress(key, adr, att, hashTableSize, *this);
+		}
+
+		if (mark[adr] == EMPTY || mark[adr] == DELETED)
+		{
+			keys[adr] = key;
+			mark[adr] = FULL;
+			values[adr] = student;
+			numberOfKeys++;
+			return true;
+		}
+		else if (keys[adr] == key) return false;
+
+		att++;
+	} while (att < hashTableSize);
+	return false;
+}
+
+void printMenu()
+{
+	cout << endl;
+	cout << "1 - Ubaci studenta" << endl;
+	cout << "2 - Izbrisi studenta" << endl;
+	cout << "3 - Pronadji studenta sa indeksom" << endl;
+	cout << "4 - Isprazni tabelu" << endl;
+	cout << "5 - Broj studenata u tabeli" << endl;
+	cout << "6 - Ispisi tabelu" << endl;
+	cout << "7 - Popunjenost" << endl;
+	cout << "0 - Kraj" << endl;
+	cout << "Uneti izbor: " << endl;
+}
+
+int main()
+{
+	HashTable* hashTable = nullptr;
+	int k, p;
+	cout << "Uneti parametre k i p: "; 
+	cin >> k >> p;
+	hashTable = new HashTable(k, p);
+	while (1)
+	{
+		printMenu();
+		int izb; cin >> izb;
+		if (izb == 1)
+		{
+			cout << "Izabrali ste ubacivanje studenta" << endl;
+			//procitaj i ubaci studenta
+		}
+		else if (izb == 2)
+		{
+			cout << "Unesite indeks studenta koji se brise: ";
+			int indeks; cin >> indeks;
+			bool del = hashTable->deleteKey(indeks);
+			if (del)
+				cout << "Student je uspesno obrisan" << endl;
+			else
+				cout << "Student sa indeksom nije ni postojao u hes tabeli." << endl;
+		}
+		else if (izb == 3)
+		{
+			cout << "Unesite indeks za pretragu: " << endl;
+			int indeks; cin >> indeks;
+			Student* stud = hashTable->findKey(indeks);
+			if (stud == nullptr)
+				cout << "Nema zadatog studenta u tabeli" << endl;
+			else
+				cout << *stud << endl;
+		}
+		else if (izb == 4)
+		{
+			cout << "Tabela ce biti ispraznjena" << endl;
+			hashTable->clear();
+		}
+		else if (izb == 5)
+		{
+			cout << "Broj studenata u tabeli je " << hashTable->keyCount() << endl;
+		}
+		else if (izb == 6)
+		{
+			cout << "Hes tabela" << endl;
+			cout << *hashTable << endl;
+		}
+		else if (izb == 7)
+		{
+			cout << "Popunjenost je " << hashTable->fillRatio() << "%" << endl;
+		}
+		else if (izb == 0)
+		{
+			hashTable->clear();
+			delete hashTable;
+			hashTable = nullptr;
+			cout << "Kraj programa" << endl;
+			break;
+		}
+		else
+		{
+			cout << "Pogresan unos";
+		}
+	}
+	return 0;
+}
