c90fd4b14c3700bacfbb43835c84efa7d75de54d
papa <pavle.jane@gmail.com>
2022-02-04 14:32:30 +0100

    service updates

--- 2.godina/1.semestar/OOP1/Vezbe/4.nedelja/Kvadar.cpp
+++
@@ -1,4 +0,0 @@
-#include "Kvadar.h"
-
-double Kvadar::Vmax = 0; // initilaztion of static params
-double Kvadar::Vuk = 0;
--- 2.godina/1.semestar/OOP1/Vezbe/4.nedelja/izvodjac.cpp
+++
@@ -1,8 +0,0 @@
-#include "izvodjac.h"
-
-string Izvodjac::str_zanr[] = { "pop", "rep", "rok" };
-
-void Izvodjac::print() const
-{
-	cout << naziv << "(" << str_zanr[zanr] << ")" << endl;
-}
